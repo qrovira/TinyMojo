3e706696dd228c4c1ab6417b2102d0fcb7acf8e2
Quim Rovira <quim@rovira.cat>
2014-04-12 15:01:20 +0200

    First commit.. it seems to work :)

---
+++ lib/App/TinyMojo.pm
@@ -0,0 +1,30 @@
+package App::TinyMojo;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Router
+  my $r = $self->routes;
+
+  # Configuration
+  $self->plugin('config');
+
+  # Database
+  $self->plugin( database => { databases => { db => $self->config->{database} } } );
+
+  # Session secret token
+  $self->secrets( $self->config->{secrets} );
+
+  # Normal route to controller
+  $r->get('/')->to('main#index');
+
+  # Actions
+  $r->post('/do/shorten')->to('main#shorten');
+
+  # Handle short url
+  $r->get('/:shorturl')->to('main#redirect');
+}
+
+1;
