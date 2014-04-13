1b4a54404934ec9931188c9efafbb7a9b0895758
Quim Rovira <quim@rovira.cat>
2014-04-13 11:56:46 +0200

    Turn the id_to_token and token_to_id methods to helpers, so they can be used all around the app

--- lib/App/TinyMojo.pm
+++ lib/App/TinyMojo.pm
@@ -1,6 +1,9 @@
 package App::TinyMojo;
 use Mojo::Base 'Mojolicious';
 
+# Could use Cryp::Skipjack for 64 bit block sizes
+use Crypt::Skip32;
+
 # This method will run once at server start
 sub startup {
   my $self = shift;
@@ -21,6 +24,10 @@ sub startup {
   # Session secret token
   $self->secrets( $self->config->{secrets} );
 
+  # Helpers to map IDs to tokens and back
+  $self->helper( id_to_token => \&_id_to_token );
+  $self->helper( token_to_id => \&_token_to_id );
+
   # Normal route to controller
   $r->get('/')->to('main#index');
 
@@ -30,5 +37,79 @@ sub startup {
   # Handle short url
   $r->get('/:shorturl')->to('main#redirect');
 }
+
+
+
+#
+# the shortening wrappers
+#
+
+sub _id_to_token {
+    my ($self, $id) = @_;
+
+    # So tiny urls can't be sequentially checked
+    $id = _encrypt( $id, $self->config->{crypt_key} );
+
+    # Now transform int to new url-friendly base
+    return _int_to_base_X( $id );
+}
+
+sub _token_to_id {
+    my ($self, $token) = @_;
+
+    # So tiny urls can't be sequentially checked
+    my $int = _base_X_to_int($token);
+
+    # Now transform int to new url-friendly base
+    return _decrypt( $int, $self->config->{crypt_key} );
+}
+
+#
+# the real stuff
+#
+
+our @BASE = ('a'..'z', 'A'..'Z', '0'..'9', qw/ $ - _ . + ! * ' ( ) /);
+
+our %REVERSE = map { $BASE[$_] => $_ } 0..$#BASE;
+
+sub _int_to_base_X($) {
+    my $int10 = shift;
+    my $intX = "";
+
+    while($int10) {
+        $intX = $BASE[ $int10 % @BASE ] . $intX;
+        $int10 = int( $int10 / @BASE );
+    }
+
+    return $intX;
+}
+
+sub _base_X_to_int($) {
+    my @intX = split '', shift;
+    my $int10 = 0;
+
+    while( my $d = shift @intX ) {
+        $int10 *= @BASE;
+        return -1 unless exists $REVERSE{ $d };
+        $int10 += $REVERSE{ $d };
+    }
+
+    return $int10;
+}
+
+sub _encrypt($$) {
+    my ($int, $key) = @_;
+    my $sj = Crypt::Skip32->new($key);
+
+    return unpack "L", $sj->encrypt( pack "L", $int );
+}
+
+sub _decrypt($$) {
+    my ($int, $key) = @_;
+    my $sj = Crypt::Skip32->new($key);
+
+    return unpack "L", $sj->decrypt( pack "L", $int );
+}
+
 
 1;
--- lib/App/TinyMojo/Main.pm
+++ lib/App/TinyMojo/Main.pm
@@ -1,15 +1,10 @@
 package App::TinyMojo::Main;
 use Mojo::Base 'Mojolicious::Controller';
 
-# Could use Cryp::Skipjack for 64 bit block sizes
-use Crypt::Skip32;
-
-
-
 sub redirect {
     my $self = shift;
     my $shorturl = $self->param('shorturl');
-    my $id = $self->_token_to_id( $shorturl );
+    my $id = $self->token_to_id( $shorturl );
     my $sth = $self->db->prepare('SELECT * FROM url WHERE id = ?');
 
     $sth->execute( $id );
@@ -27,7 +22,7 @@ sub shorten {
     my $sth = $self->db->prepare('INSERT INTO url (longurl) VALUES (?)');
 
     if( $sth->execute($longurl) ) {
-        my $token = $self->_id_to_token( $sth->{mysql_insertid} );
+        my $token = $self->id_to_token( $sth->{mysql_insertid} );
         my $shorturl = $self->url_for( '/'.$token )->to_abs;
 
         $self->respond_to( 
@@ -40,78 +35,6 @@ sub shorten {
 
 }
 
-
-#
-# the shortening wrappers
-#
-
-sub _id_to_token {
-    my ($self, $id) = @_;
-
-    # So tiny urls can't be sequentially checked
-    $id = $self->_encrypt($id);
-
-    # Now transform int to new url-friendly base
-    return $self->_int_to_base_X( $id );
-}
-
-sub _token_to_id {
-    my ($self, $token) = @_;
-
-    # So tiny urls can't be sequentially checked
-    my $int = $self->_base_X_to_int($token);
-
-    # Now transform int to new url-friendly base
-    return $self->_decrypt( $int );
-}
-
-#
-# the real stuff
-#
-
-our @BASE = ('a'..'z', 'A'..'Z', '0'..'9', qw/ $ - _ . + ! * ' ( ) /);
-
-our %REVERSE = map { $BASE[$_] => $_ } 0..$#BASE;
-
-sub _int_to_base_X {
-    my ($self, $int10) = @_;
-    my $intX = "";
-
-    while($int10) {
-        $intX = $BASE[ $int10 % @BASE ] . $intX;
-        $int10 = int( $int10 / @BASE );
-    }
-
-    return $intX;
-}
-
-sub _base_X_to_int {
-    my $self = shift;
-    my @intX = split '', shift;
-    my $int10 = 0;
-
-    while( my $d = shift @intX ) {
-        $int10 *= @BASE;
-        return -1 unless exists $REVERSE{ $d };
-        $int10 += $REVERSE{ $d };
-    }
-
-    return $int10;
-}
-
-sub _encrypt {
-    my ($self, $int) = @_;
-    my $sj = Crypt::Skip32->new($self->config->{crypt_key});
-
-    return unpack "L", $sj->encrypt( pack "L", $int );
-}
-
-sub _decrypt {
-    my ($self, $int) = @_;
-    my $sj = Crypt::Skip32->new($self->config->{crypt_key});
-
-    return unpack "L", $sj->decrypt( pack "L", $int );
-}
 
 
 1;
